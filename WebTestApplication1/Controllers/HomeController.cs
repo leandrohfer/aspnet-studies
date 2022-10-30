33683ff5c93add18757df9276dabb5a091a8df78
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-10-30 01:51:57 -0300

    Exemplo inicial aspnet core mvc
    Teste com parâmetros e models na view

---
+++ WebTestApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebTestApplication1.Models;
+
+namespace WebTestApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index(int? id)
+        {
+            return View(id);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebTestApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebTestApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
