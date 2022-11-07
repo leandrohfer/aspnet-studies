8743e61cca5fed856549226779fe5cbc042e248f
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2022-11-07 17:03:50 -0300

    Criação do projeto Sistema de Controle de Estoque

---
+++ InventoryControlSystemTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InventoryControlSystemTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
