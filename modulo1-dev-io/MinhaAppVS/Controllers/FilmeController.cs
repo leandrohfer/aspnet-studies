abd302abd12643cd15fca4df675c3ef6813793e9
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-05-29 23:11:46 -0300

    #Aula 3.9 Views Trabalhando com Formulários

---
+++ modulo1-dev-io/MinhaAppVS/Controllers/FilmeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MinhaAppVS.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinhaAppVS.Controllers
+{
+    public class FilmeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
