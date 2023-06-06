95131e12e7ec76fe442d91e51df1eb3459eb7dd5
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-06-06 15:24:15 -0300

    #Aula 3.11 Views Utilizando Scaffold para Ganhar Tempo

---
+++ modulo1-dev-io/MinhaAppVS/Data/MinhaAppVSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhaAppVS.Models;
+
+namespace MinhaAppVS.Data
+{
+    public class MinhaAppVSContext : DbContext
+    {
+        public MinhaAppVSContext (DbContextOptions<MinhaAppVSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MinhaAppVS.Models.Filme> Filme { get; set; }
+    }
+}
--- modulo1-dev-io/MinhaAppVS/Startup.cs
+++ modulo1-dev-io/MinhaAppVS/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhaAppVS.Data;
 
 namespace MinhaAppVS
 {
@@ -24,6 +26,9 @@ namespace MinhaAppVS
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+            services.AddDbContext<MinhaAppVSContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MinhaAppVSContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
