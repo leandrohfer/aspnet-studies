4e0ceb5e6d5ff8aacbbf1685877b175107304e0a
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-11-14 15:11:35 -0300

    Anotações na classe de configuração

--- DemoInicialASPNET/Program.cs
+++ DemoInicialASPNET/Program.cs
@@ -1,6 +1,22 @@
+// Configuração do Builder
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do Pipeline
+
+// Middlewares
+
+// Services 
+
+
+// Configuração da App
+
 var app = builder.Build();
 
+// Configuração de Comportamentos da App
+
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/teste", () => "Teste de mapeamento");
 
 app.Run();
