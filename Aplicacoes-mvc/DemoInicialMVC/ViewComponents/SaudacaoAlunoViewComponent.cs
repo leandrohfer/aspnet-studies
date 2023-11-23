af4e6de508ec8bf8733b2707f93e3532a4e7e7e0
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-11-23 11:08:02 -0300

    Cria exemplo de View Component e o insere na view de layout por meio de tag helpers

---
+++ Aplicacoes-mvc/DemoInicialMVC/ViewComponents/SaudacaoAlunoViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using DemoInicialMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoInicialMVC.ViewComponents
+{
+    public class SaudacaoAlunoViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var aluno = new Aluno() { Nome = "Leandro" };
+
+            return View(aluno);
+        }
+    }
+}
