71173b813acde546daf595dddaf534de01e5be50
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-11-24 10:13:57 -0300

    Implementa CRUD simples da model Aluno utilizando Entity Framework

---
+++ Aplicacoes-mvc/DemoInicialMVC/Controllers/TestesEFController.cs
@@ -0,0 +1,47 @@
+﻿using DemoInicialMVC.Data;
+using DemoInicialMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoInicialMVC.Controllers
+{
+    public class TestesEFController : Controller
+    {
+        public AppDbContext Db { get; set; }
+
+        public TestesEFController(AppDbContext db)
+        {
+            Db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var aluno = new Aluno()
+            {
+                Nome = "Leandro",
+                Email = "hello@leandro.com",
+                DataNascimento = new DateTime(2001, 06, 27),
+                Avaliacao = 5,
+                Ativo = true
+            };
+
+            // Insert
+            Db.Alunos.Add(aluno);
+            Db.SaveChanges();
+
+            // Select
+            var alunosChange = Db.Alunos.Where(a => a.Nome == "Leandro").FirstOrDefault();
+            // var alunosChange = Db.Alunos.Where(a => a.Nome.Contains("Leandro")).FirstOrDefault(); // utilizando Like
+            alunosChange.Nome = "Leandro Henrique";
+
+            // Update
+            Db.Alunos.Update(alunosChange);
+            Db.SaveChanges();
+
+            // Delete
+            Db.Alunos.Remove(alunosChange);
+            Db.SaveChanges();
+
+            return Content("Realizado!");
+        }
+    }
+}
