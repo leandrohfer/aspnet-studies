29c315259a0af0bd2352783874785d006a8dcf85
Leandro Henrique <leandro.pinheiro1427@gmail.com>
2023-11-27 15:46:36 -0300

    Configura aplicação para execução com Identity

--- Aplicacoes-mvc/DemoInicialMVC/src/AppMvcFuncional/Program.cs
+++ Aplicacoes-mvc/DemoInicialMVC/src/AppMvcFuncional/Program.cs
@@ -6,12 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+// Adiciona o Contexto do Identity, mas é como se fosse o do EF normal
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
+
+// Adiciona filtro para erros de Banco (ambiente de desenvolvimento)
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+// Adiciona o Middleware do Identity
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Adiciona MVC
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -33,11 +40,14 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// Adiciona a Autorização para caso seja solicitada na aplicação
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Adicionando RazorPages porque o componente do Identity é um Razor Pages
 app.MapRazorPages();
 
 app.Run();
